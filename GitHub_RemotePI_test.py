fdc06014081570267dc88308ad9b523cae319490
boerdejp <jordydeboer@hotmail.com>
2017-08-19 21:28:01 +0200

    GitHub_RemotePI_test.py

---
+++ GitHub_RemotePI_test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 21:03:16 2017
+
+@author: jordy
+"""
+
+git remote add origin https://github.com/boerdejp/RemotePI.git
+git push -u origin master
+
